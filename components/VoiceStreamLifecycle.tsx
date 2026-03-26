ccaa046b818f957a3971a55b270e801a36566315
IgorPots1 <potseluevigoralexeevich@gmail.com>
2026-03-26 13:04:49 +0100

    Remove pagehide mic stop to prevent iOS dialog

--- components/VoiceStreamLifecycle.tsx
+++ components/VoiceStreamLifecycle.tsx
@@ -1,20 +1,5 @@
 'use client'
 
-import { useEffect } from 'react'
-import { stopVoiceStream } from '@/lib/voice/voiceStream'
-
 export default function VoiceStreamLifecycle() {
-  useEffect(() => {
-    function handlePageHide() {
-      stopVoiceStream()
-    }
-
-    window.addEventListener('pagehide', handlePageHide)
-
-    return () => {
-      window.removeEventListener('pagehide', handlePageHide)
-    }
-  }, [])
-
   return null
 }
